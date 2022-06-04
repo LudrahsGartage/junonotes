a4a37063ace75be6e40608fa1b46d0252891da59
LudrahsGartage <shardulchaturvedi1@gmail.com>
2022-06-04 19:23:51 +0530

    Step 4 and Log Cleanup

--- lib/views/Register_View.dart
+++ lib/views/Register_View.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -53,17 +54,16 @@ class _RegisterViewState extends State<RegisterView> {
                         final email = _email.text;
                         final password = _password.text;
                         try {
-                          final userCredential = await FirebaseAuth.instance
+                          await FirebaseAuth.instance
                               .createUserWithEmailAndPassword(
                                   email: email, password: password);
                         } on FirebaseAuthException catch (e) {
                           if (e.code == 'weak-password') {
-                            print('Weak Password');
+                            devtools.log('Weak Password');
                           } else if (e.code == 'email-already-in-use') {
-                            print('The email is already in use');
+                            devtools.log('The email is already in use');
                           }
                         }
-                        print(UserCredential);
                       },
                       child: const Text('Register'),
                     ),
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -59,11 +60,11 @@ class _LoginViewState extends State<LoginView> {
                           Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false);
                         } on FirebaseAuthException catch (e) {
                           if (e.code == 'user-not-found') {
-                            print('User not found');
+                            devtools.log('User not found');
                           } else if (e.code == 'wrong-password') {
-                            print('Wrong Password');
+                            devtools.log('Wrong Password');
                           } else if (e.code == 'invalid-email') {
-                            print('invalid email entered');
+                            devtools.log('invalid email entered');
                           }
                         }
                       },
