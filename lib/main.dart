d74742d9efa9cca785d3abe17f0152db59138e88
LudrahsGartage <shardulchaturvedi1@gmail.com>
2022-06-12 16:07:11 +0530

    step 13 added new notes view, routes and button

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:junonotes/services/auth/auth_service.dart';
 import 'package:junonotes/views/login_view.dart';
 import 'package:junonotes/views/Register_View.dart';
-import 'package:junonotes/views/notes_view.dart';
+import 'package:junonotes/views/notes/new_note_view.dart';
+import 'package:junonotes/views/notes/notes_view.dart';
 import 'package:junonotes/views/verify_email_view.dart';
 import 'constants/routes.dart';
 
@@ -20,6 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
