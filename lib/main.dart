90265f6e2f9a7fbee784c034b40e8403febe08e0
LudrahsGartage <shardulchaturvedi1@gmail.com>
2022-06-21 00:10:00 +0530

    step 28 added app icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
       home: BlocProvider<AuthBloc>(
         create: (context) => AuthBloc(FirebaseAuthProvider()),
